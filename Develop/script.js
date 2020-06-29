e3c8beb8f5dbaa5e41523ec2617fdc0168fd460c
unknown <jwade1327@gmail.com>
2020-06-28 19:59:31 -0500

    password requirments

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,8 @@
 // Array Options
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharacters = ["!", "~", "/", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", ".", "-", "?"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialCharacter = ["!", "~", "/", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", ".", "-", "?"];
 
 // Prompt for password length
 function generatePassword() {
@@ -12,9 +12,16 @@ function generatePassword() {
     window.alert("Incorrect number of characters for your password. Enter a number between 8-128.");
     var passwordLength = (prompt("How many characters would you like for your password?"));
   }
-
   window.alert("OK. Your password will have " + passwordLength + " characters.");
-}
+
+// Case, numeric and special characters for password confirmation
+var lowerCase = (window.alert("Your password must contain a lower case character. Click OK."));
+var upperCase = (window.alert("Your password must contain an upper case character. Click OK."));
+var number = (window.alert("Your password must contain a number. Click OK."));
+var specialCharacter = (window.alert("Your password must contain a special character. Click OK."));
+
+
+};
  
     
     
